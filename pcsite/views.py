438a929a436e008b534bd01aeb31eecb9c00a33b
eshelton <ejs6ar@virginia.edu>
2019-09-24 14:51:28 -0400

    add about page

--- pcsite/views.py
+++ pcsite/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Here's the homepage!")
+
+def about(request):
+    return HttpResponse("Here's the about page!")
+
+# def
