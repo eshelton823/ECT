a4153e42b2de6cead32a193c5c143d1bcaaf466d
eshelton <ejs6ar@virginia.edu>
2019-09-25 00:14:05 -0400

    playing with templates and bootstrap

--- pcsite/views.py
+++ pcsite/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Here's the homepage!")
+    return render(request, 'pcsite/index.html')
+
 
 def about(request):
     return HttpResponse("Here's the about page!")
@@ -11,7 +13,7 @@ def signup(request):
     return HttpResponse("Here's the signup page!")
 
 def login(request):
-    return HttpResponse("Here's the login page!")
+    return render(request, 'pcsite/login.html')
 
 def contactus(request):
     return HttpResponse("Here's the contact us page!")
